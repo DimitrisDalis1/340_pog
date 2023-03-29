2a3e0a9b5c825af6c93beb673f30ec86f5d181bb
DimitrisDalis1 <121684210+DimitrisDalis1@users.noreply.github.com>
2023-03-29 13:54:11 +0300

    Create symtable_contains.c

---
+++ symtable_contains.c
@@ -0,0 +1,73 @@
+int SymTable_contains(SymTable_T *oSymTable, const char *pcKey, unsigned int scope){
+    int hashIndex;
+    SymbolTableEntry *temp;
+    //temp = malloc(sizeof(SymbolTableEntry*));
+    assert(oSymTable);
+    assert(pcKey);
+
+
+    /*Generate the hash index*/
+    hashIndex=SymTable_hash(pcKey,oSymTable->buckets);
+    printf("hash index inside sym_cont: %d\n", hashIndex);
+
+    /*Search if there is a cell with this key*/
+    temp = oSymTable->hashtable[hashIndex];
+    if(temp == NULL){ printf("is null\n");}
+    while (temp!=NULL)
+    {
+        //Kanoume mesa sto bucket traverse mexri na broume to idio onoma entolhs
+        
+        if(temp->type==GLOBAL||temp->type==LOCAL||temp->type==FORMAL){
+            if (strcmp(temp->value.varVal->name,pcKey) && temp->value.varVal->scope == scope)
+            {
+               return 1;
+            }
+        }else{
+            if (strcmp(temp->value.funcVal->name, pcKey) == 0 && temp->value.funcVal->scope == scope) printf("%s",pcKey);
+            if (1)
+            {
+                
+               return 1;
+            }
+        }
+        //oSymTable->size++;
+        temp= temp->next;
+    }
+    return 0;
+}
+
+int main()
+{
+    SymTable_T* hash = malloc(sizeof(SymTable_T*));
+    const char* name = "print";
+    assert(hash);
+    hash->hashtable=malloc(509 * sizeof(SymbolTableEntry*));
+    hash->buckets=509;
+    hash->size = 0;
+    SymbolTableEntry* temp_entry;
+    int hashIndex=SymTable_hash("print",hash->buckets);
+    temp_entry = (struct SymbolTableEntry*) malloc (sizeof(SymbolTableEntry));
+    temp_entry->value.funcVal=(struct Function*)malloc(sizeof(Function));
+    temp_entry->value.funcVal->name=strdup(name);
+    temp_entry->type=LIBFUNC;
+    temp_entry->value.funcVal->scope = 0;
+    hash->hashtable[hashIndex]=temp_entry;
+    
+
+
+  
+
+    id_list *id;
+    printf("%s\n", temp_entry->value.funcVal->name );
+    printf("hash index inside main: %d\n", hashIndex);
+
+    SymTable_contains(hash, "print", 0);
+    /*
+    hash = SymTable_new();
+    SymTable_insert(hash, "abcd", 1, id, 0, GLOBAL);
+    SymTable_insert(hash, "expr", 1, id, 0, LOCAL);
+    */
+//printf("%d", SymTable_getLength(hash));
+
+    return 0;
+}
