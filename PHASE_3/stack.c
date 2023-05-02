37c809009f25d4e34c14626a4c5e5b9e9739f027
nas0010 <88509829+nas0010@users.noreply.github.com>
2023-05-02 12:04:18 +0300

    Add files via upload

---
+++ PHASE_3/stack.c
@@ -0,0 +1,49 @@
+
+#define SIZE 1024
+#include <stdio.h>
+#include<stdlib.h>
+typedef struct myStruct{
+    int data;
+    myStruct* next;
+
+}myStruct;
+
+myStruct* top=NULL;
+myStruct* temp=NULL;
+myStruct* top1=NULL;
+
+int count = 0;
+void push(int data) {
+     if (top == NULL)
+    {
+        top =(struct node *)malloc(1*sizeof(myStruct));
+        top->next = NULL;
+        top->data = data;
+    }
+    else
+    {
+        temp =(struct node *)malloc(1*sizeof(myStruct));
+        temp->next = top;
+        temp->data = data;
+        top = temp;
+    }
+    count++;
+    printf("Node is Inserted\n\n");
+}
+
+int pop() {
+     top1 = top;
+ 
+    if (top1 == NULL)
+    {
+        printf("\nStack Underflow\n");
+        return -1;
+    }
+    else
+        top1 = top1->next;
+    int popped = top->data;
+    free(top);
+    top = top1;
+    count--;
+    return popped;
+}
