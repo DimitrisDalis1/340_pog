42254d68280f82c05180002ed37f7acaa1101374
DimitrisDalis1 <121684210+DimitrisDalis1@users.noreply.github.com>
2023-05-02 12:10:10 +0300

    Update quadhandler.c

--- PHASE_3/quadhandler.c
+++ PHASE_3/quadhandler.c
@@ -1,34 +1,6 @@
 #include "symtable.h"
 #include "quadhandler.h"
 
-typedef enum expr_t{
-	var_e,
-	tableitem_e,
-	programfunc_e,
-	libraryfunc_e,
-	arithexpr_e,
-	boolexpr_e,
-	assignexpr_e,
-	newtable_e,
-	constnum_e,
-	constbool_e,
-	conststring_e,
-	nil_e
-}expr_t;
-
-typedef struct expr{
-    expr_t type;
-	SymTableEntry* sym;
-	expr* index;
-	int intConst;
-	double numConst;
-	char* strConst;
-	unsigned char boolConst;
-	expr* next;
-	int truelist;
-	int falselist;
-}expr;
-
 quad*	quads = (quad*) 0;
 unsigned total = 0;
 unsigned int currQuad = 0;
