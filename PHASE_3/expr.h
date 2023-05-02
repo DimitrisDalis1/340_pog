a47df79eb208130f5b865e03e25921f0d89f2622
nas0010 <88509829+nas0010@users.noreply.github.com>
2023-05-02 12:07:13 +0300

    Update expr.h

--- PHASE_3/expr.h
+++ PHASE_3/expr.h
@@ -1,1 +1,36 @@
 
+typedef enum expr_t{
+	var_e,
+	tableitem_e,
+	programfunc_e,
+	libraryfunc_e,
+	arithexpr_e,
+	boolexpr_e,
+	assignexpr_e,
+	newtable_e,
+	constnum_e,
+	constbool_e,
+	conststring_e,
+	nil_e
+}expr_t;
+
+typedef struct expr{
+    expr_t type;
+	SymTableEntry* sym;
+	expr* index;
+	int intConst;
+	double numConst;
+	char* strConst;
+	unsigned char boolConst;
+	expr* next;
+	int truelist;
+	int falselist;
+}expr;
+
+expr* newexpr(expr_t t);
+expr* newexpr_conststring(char* s);
+expr* newexpr_constdouble(double i);
+expr* newexpr_constnil();
+expr* newexpr_constbool(unsigned char b);
+expr* newexpr_constint(int i);
+expr* lvalue_expr(SymTableEntry* sym);
