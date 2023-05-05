1a49677b8d7e36885479b5baa6de71793701a94e
nas0010 <88509829+nas0010@users.noreply.github.com>
2023-05-05 14:43:57 +0300

    Update expr.c

--- PHASE_3/expr.c
+++ PHASE_3/expr.c
@@ -1,48 +1,36 @@
+#include "expr.h"
 expr* newexpr(expr_t t){
 	expr* e=(expr*) malloc(sizeof(expr));
 	memset(e,0,sizeof(expr));
 	e->type=t;
 	e->truelist = 0;
-    e->falselist = 0;
+        e->falselist = 0;
 	return e;
 }
 
 expr* newexpr_conststring(char* s){
 	expr* e=newexpr(conststring_e);
 	e->strConst= strdup(s);
-	e->doubleConst = NULL;
-	e->boolConst = NULL;
-	e->intConst = NULL;
-	e->sym = NULL;
+		e->sym = NULL;
 	return e;
 }
 
 expr* newexpr_constdouble(double i){
     expr* e = newexpr(constdouble_e);
-    e->doubleConst = i;
-	e->strConst= NULL;
-	e->boolConst = NULL;
-	e->intConst = NULL;
+    e->numConst = i;
 	e->sym = NULL;
     return e;
 }
 
 expr* newexpr_constnil(){
     expr* e = newexpr(nil_e);
-	e->doubleConst = NULL;
-	e->strConst= NULL;
-	e->boolConst = NULL;
-	e->intConst = NULL;
-	e->sym = NULL;
+		e->sym = NULL;
     return e;
 }
 
 expr* newexpr_constbool(unsigned char b){
     expr* e = newexpr(constbool_e);
     e->boolConst = b;
-	e->doubleConst = NULL;
-	e->strConst= NULL;
-	e->intConst = NULL;
 	e->sym = NULL;
     return e;
 }
@@ -50,14 +38,11 @@ expr* newexpr_constbool(unsigned char b){
 expr* newexpr_constint(int i){
     expr* e = newexpr(constint_e);
     e->intConst = i;
-	e->doubleConst = NULL;
-	e->strConst= NULL;
-	e->boolConst = NULL;
 	e->sym = NULL;
     return e;
 }
 
-expr* lvalue_expr(SymTableEntry* sym){
+expr* lvalue_expr(SymbolTableEntry* sym){
 	assert(sym);
 	expr* e=(expr*)malloc(sizeof(expr));
 	memset(e,0,sizeof(expr));
