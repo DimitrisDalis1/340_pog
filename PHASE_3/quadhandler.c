80dbf1ba026f5fedbcdeca0b6992d802a3a2e8bb
nas0010 <88509829+nas0010@users.noreply.github.com>
2023-05-11 17:49:34 +0300

    Update quadhandler.c

--- PHASE_3/quadhandler.c
+++ PHASE_3/quadhandler.c
@@ -8,10 +8,9 @@ unsigned programVarOffset =0;
 unsigned functionLocalOffset=0;
 unsigned formalArgOffset=0;
 unsigned scopeSpaceCounter=1;
-
 void printExpr(expr* argExpr){
 	if(argExpr==NULL){
-		printf("keno expr");
+		printf("%-*s \t",10,"\t");
 	}else{
 		if(argExpr->type == var_e){
 				printf("%-*s \t",20,argExpr->sym->value.varVal->name);
@@ -40,22 +39,23 @@ void printExpr(expr* argExpr){
 			}else if (argExpr->type == conststring_e){
 				printf("%-*s \t",20,argExpr->strConst);
 			}else if (argExpr->type == nil_e){
-				printf("NIL \t");
+				printf("%-*s \t",20,"NIL");
 			}
 	}
 
 
 }
 void printMedianCode(){
-    char opcode_array[25][25]={
+    char opcode_array[26][25]={
 		"assign\0","add\0","sub\0",
 		"mul\0","div\0","mod\0",
 		"uminus\0","and\0","or\0",
-		"not\0","if_eq\0","if_noteq\0",
-		"if_greater\0","call\0","param\0",
+		"not\0","if_eq\0","if_noteq\0","if_greater\0",
+		"if_lesseq\0", "if_less\0",
+		"if_greatereq\0","call\0","param\0",
 		"ret\0","getretval\0","funcstart\0",
 		"funcend\0","tablecreate\0","tablegetelem\0",    
-		"tablesetelem\0"};
+		"tablesetelem\0","jump\0"};
 
     int line_for_print = 0;
     printf("quad#\t\t\topcdode\t\t\tresult\t\t\targ1\t\t\targ2\t\t\tlabel\n");
@@ -68,33 +68,33 @@ void printMedianCode(){
 		 if(quads[i].op== assign){
 			printExpr(quads[i].result);
 			printExpr(quads[i].arg1);
-			printf("%-*s \t",20,"\t");
+			printf("%-*s \t",15,"\t");
         }else if(quads[i].op == add || quads[i].op== sub || quads[i].op == mul || quads[i].op == divi || quads[i].op== mod){
-            printExpr(quads[i].result);
+            		printExpr(quads[i].result);
 			printExpr(quads[i].arg1);
 			printExpr(quads[i].arg2);
 	    }else if(quads[i].op == uminus || quads[i].op == not){
-            printExpr(quads[i].result);
+            		printExpr(quads[i].result);
 			printExpr(quads[i].arg1);
 			printf("%-*s \t",20,"\t");
 	    }else if(quads[i].op == and ||quads[i].op== or ){
-		    printExpr(quads[i].result);
+		    	printExpr(quads[i].result);
 			printExpr(quads[i].arg1);
 			printExpr(quads[i].arg2);
 	    }else if(quads[i].op== if_eq || quads[i].op== if_noteq || quads[i].op == if_lesseq || quads[i].op== if_greatereq || quads[i].op == if_less || quads[i].op == if_greater){
-            printExpr(quads[i].result);
+            		printExpr(quads[i].result);
 			printExpr(quads[i].arg1);
 			printExpr(quads[i].arg2);
         }else if(quads[i].op == call || quads[i].op == jump || quads[i].op == tablecreate || quads[i].op == funcend || quads[i].op == getretval || quads[i].op == funcstart || quads[i].op== param || quads[i].op== ret){
-		    printExpr(quads[i].result);
+		    	printExpr(quads[i].result);
 			printf("%-*s \t",20,"\t");
-			printf("%-*s \t",20,"\t");
+			printf("%-*s \t",5,"\t");
 	    }else if(quads[i].op == tablegetelem){
-            printExpr(quads[i].result);
+            		printExpr(quads[i].result);
 			printExpr(quads[i].arg1);
 			printExpr(quads[i].arg2);       
         }else if(quads[i].op == tablesetelem){
-            printExpr(quads[i].result);
+           		 printExpr(quads[i].result);
 			printExpr(quads[i].arg1);
 			printExpr(quads[i].arg2);  
 	    }
@@ -105,7 +105,6 @@ void printMedianCode(){
     }
     return;
 }
-
 
 scopespace_t currscopespace(){
 	if(scopeSpaceCounter==1){
